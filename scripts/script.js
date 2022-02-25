4dd498a39cef2d8d0a79cd93de0a2487265c10d9
Artur <arturdantasrodrigues@gmail.com>
2022-02-24 22:19:40 -0300

    algumas coisas melhoraram, varias pioraram
    tive alguns problemas com a lógica, por isso desliguei as interações com contas, mas o usuário ainda pode ligar a calculadora e digitar algumas coisas, mas por enquanto nada de contas

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
+let ligado = false
 let displayNumber = ""
+let nameIt = 0 // variavel que armazena o valor da tela, fiquei sem ideia pra nomear ela
 let inputs = {
    values: [], 
-   operations: []
+   operations: ""
 }
 let conta = 0
 let apaga = false
@@ -12,77 +14,151 @@ function render() {
 }
 
 function concatena(number) {
-   if(displayNumber === "0" && number === "0" || apaga === true){
-      displayNumber = ""
-      if(apaga === true) {
-         displayNumber += number
-         apaga = false
+   if(ligado === true){
+      if(displayNumber === "0"){
+      apaga = true
       }
-      render()
-   } else if (displayNumber.length < 7){
-      displayNumber += number
-      render()
+      if(displayEl === "0" || apaga === true){
+      displayNumber = ""
+         if(apaga === true) {
+            displayNumber += number
+            nameIt = parseFloat(displayNumber)
+            apaga = false
+         }
+         render()
+      } else if (displayNumber.length < 7){
+         displayNumber += number
+         nameIt = parseFloat(displayNumber)
+         render()
+      }
    }
+   
    
 }
 
 function somar() {
-   inputs.values.push(parseFloat(displayNumber))
-   inputs.operations.push("+")
-   displayNumber = ""
-   console.log(conta)
-   render()
-   
+   if(ligado){
+      alert("Ainda não funciona direito")
+   } 
+   // inputs.operations = "+"
+   // if(inputs.values.length === 2) {
+   //    inputs.values[0] = inputs.values[1]
+   //    inputs.values[1] = nameIt
+   //    calcular()
+   // } else {
+   //    inputs.values.push(nameIt)
+   //    displayNumber = ""
+   //    console.log(conta)
+
+   //    render()
+   // }  
 }
 function subtrair() {
-   inputs.values.push(parseFloat(displayNumber))
-   inputs.operations.push("-")
-   displayNumber = ""
-   console.log(conta)
-   render()
+   if(ligado){
+      alert("Ainda não funciona direito")
+   } 
+   // inputs.operations = "-"
+   // if(inputs.values.length === 2) {
+   //    inputs.values[0] = inputs.values[1]
+   //    inputs.values[1] = nameIt
+   //    calcular()
+   // } else {
+   //    inputs.values.push(nameIt)
+   //    displayNumber = ""
+   //    console.log(conta)
+   //    render()
+   // }
    
 }
 function multiplicar() {
-   inputs.values.push(parseFloat(displayNumber))
-   inputs.operations.push("*")
-   displayNumber = ""
-   console.log(conta)
-   
+   if(ligado){
+      alert("Ainda não funciona direito")
+   } 
+   // inputs.operations = "*"
+   // if(inputs.values.length === 2) {
+   //    inputs.values[0] = nameIt
+   //    calcular()
+   // } else {
+   //    inputs.values.push(nameIt)
+   //    displayNumber = ""
+   //    console.log(conta)
+   //    render()
+   // }
 }
 function dividir() {
-   inputs.values.push(parseFloat(displayNumber))
-   inputs.operations.push("/")
-   displayNumber = ""
-   console.log(conta)
-   
+   if(ligado){
+      alert("Ainda não funciona direito")
+   } 
+// inputs.operations = "/"
+   // if(inputs.values.length === 2) {
+   //    inputs.values[0] = inputs.values[1]
+   //    inputs.values[1] = nameIt
+   //    calcular()
+      
+   // } else {
+   //    inputs.values.push(nameIt)
+   //    displayNumber = ""
+   //    console.log(conta)
+   //    render()
+   // }
 }
 
-function calcular(){
-   inputs.values.push(parseFloat(displayNumber))
-   switch(inputs.operations[0]){
-      case '+': 
-      conta = inputs.values[0] + inputs.values[1] 
-         break; 
-      case '/': 
-         conta = inputs.values[0] / inputs.values[1] 
-         break; 
-      case '*': 
-         conta = inputs.values[0] * inputs.values[1] 
-         break; 
-      case '-': 
-         conta = inputs.values[0] - inputs.values[1] 
-         break; 
+function calcular(limpa=false){
+   if(ligado){
+      alert("Ainda não funciona direito")
+   } 
+   // if(inputs.values.length < 2){
+   //    inputs.values.push(nameIt)
+   // }
+   // switch(inputs.operations){
+   //    case '+': 
+   //       conta = inputs.values[0] + inputs.values[1] 
+   //       break; 
+   //    case '/': 
+   //       conta = inputs.values[0] / inputs.values[1] 
+   //       break; 
+   //    case '*': 
+   //       conta = inputs.values[0] * inputs.values[1] 
+   //       break; 
+   //    case '-': 
+   //       conta = inputs.values[0] - inputs.values[1] 
+   //       break; 
+   // }  
+   // displayNumber = conta
+   // if(inputs.values.length == 2){
+   //    inputs.values[1] = conta
+   // }
+   // apaga = true
+   // if(limpa === true){
+   //    limpar(true)
+   // }
+   // render()
+}
+
+function limpar(zero=true) {
+   if (zero === true){
+      displayNumber = "0"
+   } else {
+      displayNumber = ""
    }
-   displayNumber = conta
    conta = 0
    inputs.values = []
    inputs.operations = []
-   apaga = true
    render()
 }
 
-function limpar() {
-   displayNumber = ""
-   conta = 0
-   render()
+function makeItZero() {
+   if(ligado){
+      limpar(true)
+   }
+}
+
+function turnOn() {
+   limpar(true)
+   ligado = true
+}
+
+function turnOff() {
+   limpar(false)
+   ligado = false
 }
